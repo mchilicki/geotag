df1585204289de47f6251c44a71c5ccdcd8cd76f
LukaszBlasiak <lukasz.blasiak@wp.pl>
2019-03-24 21:59:14 +0100

    Fix cardinal direction

--- src/app/models/gps-exif-info.ts
+++ src/app/models/gps-exif-info.ts
@@ -1,7 +1,7 @@
 export class GpsExifInfo {
   latitude: string;
   longitude: string;
-  constructor(latitude: string, longitude: string){
+  constructor(latitude: string, longitude: string) {
     this.latitude = latitude;
     this.longitude = longitude;
   }

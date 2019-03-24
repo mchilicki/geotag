4f755ef89e6397bda82a6835aa8269c44b05312c
LukaszBlasiak <lukasz.blasiak@wp.pl>
2019-03-24 21:39:24 +0100

    Add read exif gps feature

---
+++ src/app/models/gps-exif-info.ts
@@ -0,0 +1,8 @@
+export class GpsExifInfo {
+  latitude: string;
+  longitude: string;
+  constructor(latitude: string, longitude: string){
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+}

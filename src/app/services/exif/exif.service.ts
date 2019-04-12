6902e08fb5646e0eb534ce11151e977284a21dc8
LukaszBlasiak <lukasz.blasiak@wp.pl>
2019-04-12 09:30:42 +0200

    Add update exif feature and file save

--- src/app/services/exif/exif.service.ts
+++ src/app/services/exif/exif.service.ts
@@ -9,7 +9,7 @@ export class ExifService {
   constructor(private electronService: ElectronService) {
   }
 
-  public getExifGps(imagePath: string) {
+  public getExifGps(imagePath: string): ExifGpsInfo {
     let gpsExifInfo;
     try {
       const exifObject = this.getExifObject(imagePath);
@@ -30,6 +30,30 @@ export class ExifService {
     return this.containExifGpsSection(exifObject);
   }
 
+  public setExifGps(exifGpsInfo: ExifGpsInfo, imagePath: string) {
+    const exifObject = this.getExifObject(imagePath);
+    console.log(exifObject);
+    exifObject.GPS[1] = Number(exifGpsInfo.latitude) < 0 ? 'W' : 'E';
+    exifObject.GPS[2] = this.DegToDmsRational(Number(exifGpsInfo.latitude));
+    exifObject.GPS[3] = Number(exifGpsInfo.longitude) < 0 ? 'S' : 'N';
+    exifObject.GPS[4] = this.DegToDmsRational(Number(exifGpsInfo.longitude));
+    const exifBytes = piexif.dump(exifObject);
+    this.updateImageExifSection(exifBytes, imagePath, exifObject);
+  }
+
+  private updateImageExifSection(exifBytes: any, imagePath: string, exifObject: any) {
+    const imageAsBase64 = this.base64_encode(imagePath);
+    let base64Img: string = piexif.insert(exifBytes, 'data:image/jpeg;base64,' + imageAsBase64);
+    base64Img = base64Img.substr(22);
+    this.updateImageFile(base64Img, imagePath);
+}
+
+  private updateImageFile(data: string, imagePath: string) {
+    const fs = this.electronService.remote.require('fs');
+    const buffer = Buffer.from(data, 'base64');
+    fs.writeFileSync(imagePath, buffer);
+  }
+
   private containExifGpsSection(exifObject): boolean {
     return exifObject !== 'undefined' && exifObject.GPS !== 'undefined' && typeof exifObject.GPS[1] !== 'undefined';
   }
@@ -48,6 +72,15 @@ export class ExifService {
   }
 
 
+  private DegToDmsRational(degrees: number) {
+    degrees = degrees > 0 ? degrees : -degrees;
+    const minFloat = degrees % 1 * 60;
+    const secFloat = minFloat % 1 * 60;
+    const deg = Math.floor(degrees);
+    const min = Math.floor(minFloat);
+    const sec = Math.round(secFloat * 10000);
+    return [[deg, 1], [min, 1], [sec, 10000]];
+  }
 
   private dmsRationalToDeg(cardinalDirection, coordinates) {
     const deg = coordinates[0][0] / coordinates[0][1];
